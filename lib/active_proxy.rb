a7247f2007963068c13b56b8d2f8cb59490be06b
sebi <gore.sebyx@yahoo.com>
2019-03-03 01:41:36 +0200

    change active proxy

--- lib/active_proxy.rb
+++ lib/active_proxy.rb
@@ -1,5 +1,18 @@
-require "active_proxy/version"
+# frozen_string_literal: true
+
+require "zeitwerk"
+require "retries"
+require "proxy_fetcher"
+require "user_agent_randomizer"
+
+Zeitwerk::Loader.for_gem.setup
 
 module ActiveProxy
-  # Your code goes here...
+  class << self
+    def call(proxy_key, cache_store, options = {})
+      ActiveProxy::Fetcher.new(proxy_key, cache_store, options).send(:call) do |fetcher|
+        yield fetcher
+      end
+    end
+  end
 end
