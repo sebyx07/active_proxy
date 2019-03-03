618c4bd585d89e175bc97d3c247206c7a6ceb4bd
sebi <gore.sebyx@yahoo.com>
2019-03-04 01:41:59 +0200

    change http spec

---
+++ spec/http_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe HTTP do
+  it "can fetch" do
+    ActiveProxy.call("shared", SPEC_CACHE) do |proxy|
+      proxy_arguments = proxy.format_proxy_http
+      headers = {
+        "Accept" => "application/json",
+        "User-Agent" => proxy.user_agent
+      }
+
+      result = HTTP.via(*proxy_arguments)
+                   .headers(headers)
+                   .timeout(write: 2, connect: 1, read: 1)
+                   .get("https://api.ipify.org?format=json")
+                   .body
+
+      result = JSON.parse(result)
+      raise "not match" unless result["ip"] == proxy.current_proxy[:address]
+
+      expect(result["ip"]).to eq(proxy.current_proxy[:address])
+    end
+  end
+end
