341bff3834b939b64efb6c54b371d92e197bb032
sebi <gore.sebyx@yahoo.com>
2019-03-03 04:47:56 +0200

    change spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
-require "bundler/setup"
+# frozen_string_literal: true
+
 require "active_proxy"
+require "typhoeus"
+require "http"
+require "httparty"
+require "active_support/all"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -8,7 +13,12 @@ RSpec.configure do |config|
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
+  config.expose_dsl_globally = true
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
 end
+
+ENV["LOG_ACTIVE_PROXY"] = "true"
+SPEC_CACHE ||= ActiveSupport::Cache::MemoryStore.new(size: 10.megabytes)
