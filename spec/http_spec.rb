c558e901f679b8bc738a73e526f5f1834aed175e
sebi <gore.sebyx@yahoo.com>
2019-03-06 08:39:55 +0200

    fix http spec

--- spec/http_spec.rb
+++ spec/http_spec.rb
@@ -4,8 +4,9 @@ require "spec_helper"
 
 describe HTTP do
   it "can fetch" do
-    ActiveProxy.call("shared", SPEC_CACHE) do |proxy|
+    ActiveProxy.call("shared", SPEC_CACHE, unique_per_process: true) do |proxy|
       proxy_arguments = proxy.format_proxy_http
+      p proxy_arguments
       headers = {
         "Accept" => "application/json",
         "User-Agent" => proxy.user_agent
